099cb25626fcdc453e91caa0621b0f77e0effcaa
yurikkon <yuriy.doss@gmail.com>
2022-11-07 18:20:20 +0200

    renamed

---
+++ Lesson_1/task5.py
@@ -0,0 +1,4 @@
+spisok = input("Write your spisok: ").split(",")
+max_chislo = max(spisok)
+min_chislo = min(spisok)
+print("max " + str(max_chislo + " min " + str(min_chislo)))
--- Lesson_2/task3.py
+++ Lesson_2/task3.py
@@ -1,5 +1,5 @@
 size = int(input("write number"))
 for size in range(1, size+1):
-    for column in range(size, 0, -1 ):
+    for column in range(size, 0, - 1 ):
         print(column, end=' ')
     print("")
