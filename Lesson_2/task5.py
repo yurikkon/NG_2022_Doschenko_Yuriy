80d18d21e154dcab21ef73c014ce876475aea5ff
yurikkon <yuriy.doss@gmail.com>
2022-11-18 13:34:34 +0200

    made changes

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -1,10 +1,10 @@
-spisok = input("Write your spisok: ").split(",")
+spisok1 = input("Write your spisok: ").split(",")
 changedSpisok = []
-for elements in spisok:
+for elements in spisok1:
     changedSpisok.append(int(elements))
-max_chislo = max(changedSpisok)
-min_chislo = min(changedSpisok)
-lishnee =   max_chislo + min_chislo
-summa = sum(changedSpisok) - lishnee
+max_chislo = int(max(changedSpisok))
+min_chislo = int(min(changedSpisok))
+changedSpisok.sort()
+summa = sum(changedSpisok[1:-1])
 print("summa last" + str(summa))
 print("max " + str(max_chislo) + " min " + str(min_chislo))
